cce9b81168697494d0bc761ed47fee0e39c73a96
toxiccclub <150171690+toxiccclub@users.noreply.github.com>
2024-05-21 12:06:48 +0200

    Update testTracker.cpp

--- tests/testTracker.cpp
+++ tests/testTracker.cpp
@@ -17,8 +17,6 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <gtest/gtest.h>
-#include <opencv2/opencv.hpp>
-#include <opencv2/video/tracking.hpp>
 
 #include <algorithm>
 #include <cstdlib>
@@ -134,7 +132,7 @@ class TestTracker : public ::testing::Test {
         std::make_shared<VIO::StereoCamera>(cam_params_left, cam_params_right);
     stereo_matcher_ = std::make_unique<VIO::StereoMatcher>(
         stereo_camera_, tp.stereo_matching_params_);
-    tracker_ = std::make_unique<VIO::Tracker>(
+    tracker_ = std::make_unique<Tracker>(
         tracker_params_, stereo_camera_->getOriginalLeftCamera());
 
     feature_detector_ =
@@ -690,7 +688,7 @@ class TestTracker : public ::testing::Test {
  protected:
   // Perform Ransac
   TrackerParams tracker_params_;
-  VIO::Tracker::UniquePtr tracker_;
+  Tracker::UniquePtr tracker_;
   VIO::FeatureDetector::UniquePtr feature_detector_;
 
   Frame::Ptr ref_frame, cur_frame;
@@ -778,7 +776,7 @@ TEST_F(TestTracker, geometricOutlierRejection2d2d) {
       trackerParams.ransac_max_iterations_ = 1000;
       // trackerParams.ransac_probability_ = 0.8;
       trackerParams.ransac_randomize_ = false;
-      VIO::Tracker tracker(trackerParams, stereo_camera_->getOriginalLeftCamera());
+      Tracker tracker(trackerParams, stereo_camera_->getOriginalLeftCamera());
       TrackingStatus tracking_status;
       Pose3 estimated_pose;
       tie(tracking_status, estimated_pose) =
@@ -807,7 +805,7 @@ TEST_F(TestTracker, geometricOutlierRejection2d2dGivenRotation) {
   TrackerParams tracker_params = TrackerParams();
   tracker_params.ransac_use_1point_stereo_ = true;
   tracker_params.ransac_randomize_ = false;
-  VIO::Tracker tracker(tracker_params, stereo_camera_->getOriginalLeftCamera());
+  Tracker tracker(tracker_params, stereo_camera_->getOriginalLeftCamera());
   // Start with the simplest case:
   // Noise free, no outlier, non-planar
 
@@ -977,7 +975,7 @@ TEST_F(TestTracker, geometricOutlierRejection3d3d) {
       TrackerParams trackerParams;
       trackerParams.ransac_threshold_stereo_ = 0.3;
       trackerParams.ransac_randomize_ = false;
-      VIO::Tracker tracker(trackerParams, stereo_camera_->getOriginalLeftCamera());
+      Tracker tracker(trackerParams, stereo_camera_->getOriginalLeftCamera());
       TrackingStatus tracking_status;
       Pose3 estimated_pose;
       tie(tracking_status, estimated_pose) =
@@ -1218,7 +1216,7 @@ TEST_F(TestTracker, getPoint3AndCovariance) {
   // use function to get actual answer
   Vector3 f_ref_i_expected, f_ref_i_actual;
   Matrix3 cov_ref_i_expected, cov_ref_i_actual;
-  tie(f_ref_i_actual, cov_ref_i_actual) = tracker_.getPoint3AndCovariance(
+  tie(f_ref_i_actual, cov_ref_i_actual) = Tracker::getPoint3AndCovariance(
       *ref_stereo_frame, stereoCam, pointId, stereoPtCov);
 
   // use monte carlo method to get expected answer
@@ -1263,7 +1261,7 @@ TEST_F(TestTracker, findOutliers) {
     random_shuffle(inliers.begin(), inliers.end());
 
     vector<int> outliers_actual;
-    VIO::Tracker::findOutliers(matches_ref_cur, inliers, &outliers_actual);
+    Tracker::findOutliers(matches_ref_cur, inliers, &outliers_actual);
 
     // check that outliers_actual matches outliers_expected
     EXPECT_EQ(outliers_expected.size(), outliers_actual.size());
@@ -1291,7 +1289,7 @@ TEST_F(TestTracker, findOutliers) {
     vector<pair<size_t, size_t>> matches_ref_cur(num_outliers);
 
     vector<int> outliers_actual;
-    VIO::Tracker::findOutliers(matches_ref_cur, inliers, &outliers_actual);
+    Tracker::findOutliers(matches_ref_cur, inliers, &outliers_actual);
 
     // check that outliers_actual matches outliers_expected
     EXPECT_EQ(outliers_expected.size(), outliers_actual.size());
@@ -1318,7 +1316,7 @@ TEST_F(TestTracker, findOutliers) {
     random_shuffle(inliers.begin(), inliers.end());
 
     vector<int> outliers_actual;
-    VIO::Tracker::findOutliers(matches_ref_cur, inliers, &outliers_actual);
+    Tracker::findOutliers(matches_ref_cur, inliers, &outliers_actual);
 
     // check that outliers_actual matches outliers_expected
     EXPECT_EQ(outliers_actual.size(), 0);
@@ -1368,7 +1366,7 @@ TEST_F(TestTracker, FindMatchingKeypoints) {
   random_shuffle(cur_frame->landmarks_.begin(), cur_frame->landmarks_.end());
 
   vector<pair<size_t, size_t>> matches_ref_cur;
-  VIO::Tracker::findMatchingKeypoints(*ref_frame, *cur_frame, &matches_ref_cur);
+  Tracker::findMatchingKeypoints(*ref_frame, *cur_frame, &matches_ref_cur);
 
   // Check the correctness of matches_ref_cur
   EXPECT_EQ(matches_ref_cur.size(), num_landmarks_common);
@@ -1457,7 +1455,7 @@ TEST_F(TestTracker, FindMatchingStereoKeypoints) {
   }
 
   vector<pair<size_t, size_t>> matches_ref_cur;
-  VIO::Tracker::findMatchingStereoKeypoints(
+  Tracker::findMatchingStereoKeypoints(
       *ref_stereo_frame, *cur_stereo_frame, &matches_ref_cur);
 
   // Check the correctness!
@@ -1636,7 +1634,7 @@ TEST_F(TestTracker, PnPTracking) {
   tracker_params_.ransac_threshold_pnp_ = 0.5;
 
   //! Create pnp tracker
-  tracker_ = std::make_unique<VIO::Tracker>(tracker_params_,
+  tracker_ = std::make_unique<Tracker>(tracker_params_,
                                        stereo_camera_->getOriginalLeftCamera());
 
   //! Populate data
